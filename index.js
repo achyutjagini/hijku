82503b819c81bf4c9a4f6857cc9614c3444e23ac
achyutjagini <achyut.jagini@gmail.com>
2021-06-02 13:03:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-//const path = require("path")
+const path = require("path")
 
 
 const db = require('./db')
@@ -13,7 +13,7 @@ const apiPort = process.env.PORT||8000
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
-//app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
 app.use('/api', movieRouter)
 
 
-//app.get("*", (req, res) => {
-  //  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-//});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
