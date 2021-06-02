fbce6f8164aa69a88a05a22c7ae95eea7e0c0ca6
achyutjagini <achyut.jagini@gmail.com>
2021-06-02 12:47:49 +0530

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('process.env.MONGODB_URI',{ useNewUrlParser: true })
+    .connect('mongodb://achyutjjj:abcde@cluster0-shard-00-00.yk9zd.mongodb.net:27017,cluster0-shard-00-01.yk9zd.mongodb.net:27017,cluster0-shard-00-02.yk9zd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-3nab45-shard-0&authSource=admin&retryWrites=true&w=majority',{ useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
