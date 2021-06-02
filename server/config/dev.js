06362c0a6640a70f91d7d3bd689cce2334b3045a
achyutjagini <achyut.jagini@gmail.com>
2021-06-02 11:33:51 +0530

    Delete server directory

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports=
-{
-    mongourl:"mongodb://achyutjjj:abcde@cluster0-shard-00-00.yk9zd.mongodb.net:27017,cluster0-shard-00-01.yk9zd.mongodb.net:27017,cluster0-shard-00-02.yk9zd.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-3nab45-shard-0&authSource=admin&retryWrites=true&w=majority"
-}
--- server/config/prod.js
+++
@@ -1,4 +0,0 @@
-module.exports=
-{
-    mongourl:process.env.MONGOURL
-}
