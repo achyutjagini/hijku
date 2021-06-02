e5eb68f3af509b2b425a18d7667feb17e98bef19
achyutjagini <achyut.jagini@gmail.com>
2021-06-02 19:38:41 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 require('dotenv').config();
 
 const api = axios.create({
-    baseURL: 'process.env.BASEURL',
+    baseURL: 'http://localhost:8000/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
