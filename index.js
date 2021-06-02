12781d02ff9ae6736e168ad1026bf1019a2c3526
achyutjagini <achyut.jagini@gmail.com>
2021-06-02 12:18:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const db = require('./db')
 const movieRouter = require('./routes/movie-router')
 
 const app = express()
-const apiPort = process.env.PORT||3000
+const apiPort = process.env.PORT||8000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
